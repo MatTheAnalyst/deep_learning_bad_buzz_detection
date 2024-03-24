982cbc078ad5b764d96e0b894a65f36c2b51063f
MatTheAnalyst <matthieu92400@icloud.com>
2024-03-24 19:57:06 +0100

    Ajout tests unitaires

---
+++ tests/test_text_preprocessing.py
@@ -0,0 +1,24 @@
+from src.text_preprocessing import Text_preprocessing
+import pandas as pd
+import numpy as np
+
+def test_should_clean_comment():
+    # sut = system under test
+    sut = Text_preprocessing()
+    initial_comment = "@switchfoot http://twitpic.com/2y1zl - Awww, that's a bummer.  You shoulda got David Carr of Third Day to do it. ;D"
+    expected_comment = "awww that bummer you shoulda got david carr of third day to do it"
+    assert sut.clean_comment(initial_comment) == expected_comment
+
+def test_should_sample_dataset():
+    sut = Text_preprocessing()
+    initial_values = pd.Series(np.random.randint(100, size=(100)))
+    initial_labels = pd.Series(np.random.randint(2, size=(100)))
+    expected_sampling_shape = 10
+    values_sample, labels_sample = sut.sampling(sample_size=10, values=initial_values, labels=initial_labels)
+    assert len(values_sample) == 10
+
+def test_should_lemmatize_comment():
+    sut = Text_preprocessing()
+    initial_comment = "I couldn't bear to watch it. And I thought the UA loss was embarrassing"
+    expected_comment = "I could n't bear watch . And I think UA loss embarrass"
+    assert sut.lemmatized_comment(initial_comment) == expected_comment
